fc0f03688dbb861376aef17ddbbc30070242a886
Vihaan Mehta <133607593+SoftwareDeveloperVM@users.noreply.github.com>
2026-03-03 23:07:45 -0800

    Refactor database handling and table management

--- main.py
+++ main.py
@@ -1,54 +1,34 @@
 import sqlite3
 
-def init(name: str):
-    global database, cursor
-    database = sqlite3.connect(f"{name}.db")
-    cursor = database.cursor()
+def run(cursor, query):
+    cursor.execute(query)
 
-def run(command: str):
-    cursor.execute(command)
-
-def close():
+def close(database):
     database.close()
 
-def show_tables():
-    tables = []
-    cursor.execute("""SELECT name FROM sqlite_master WHERE type='table'""")
-    unformat_tables = cursor.fetchall()
-    for item in unformat_tables:
-        tables.append(item[0])
-    return tables
-
-def table_exist(table: str):
-    if table in show_tables():
-        return "exists"
-    else:
-        return "no exist"
-
-class invalid_parameter(Exception):
-    pass
-
-class table:
-    def __init__(self, name, columns: list, data_types: list):
-        self.name, self.columns, self.data_types = name, columns, data_types
-        if len(data_types) == 1:
-            temp_list = []
-            for data_type in range(0, len(columns), 1):
-                temp_list.append(self.data_types[0])
-            self.data_types = temp_list
-        elif len(columns) != len(data_types):
-            raise invalid_parameter("invalid number of data types")
+class database:
+    def __init__(self, name):
+        self.name, self.query = name, ""
+        self.database = sqlite3.connect(f"{name}.db")
+        self.cursor = self.database.cursor()
 
     def __str__(self):
-        return f"Table Name: {self.name}"
+        return f"database name: {self.name}"
 
-    def create(self):
-        query = f"CREATE TABLE {self.name} (\n"
-        for i in range(0, len(self.columns), 1):
-            query += f"{self.columns[i]} {self.data_types[i]}"
-            if i == len(self.columns)-1:
-                query += "\n)"
+    def show_tables(self):
+        tables = []
+        self.cursor.execute("""SELECT name FROM sqlite_master WHERE type='table'""")
+        unformat_tables = self.cursor.fetchall()
+        for item in unformat_tables:
+            tables.append(item[0])
+        return tables
+
+    def build_table(self, name, columns: list, data_types: list):
+        self.query = f"CREATE TABLE {self.name} (\n"
+        for i in range(0, len(columns), 1):
+            self.query += f"{columns[i]} {data_types[i]}"
+            if i == len(columns) - 1:
+                self.query += "\n)"
             else:
-                query += "\n"
-        run(query)
-        print(query)
+                self.query += "\n"
+        run(self.cursor, self.query)
